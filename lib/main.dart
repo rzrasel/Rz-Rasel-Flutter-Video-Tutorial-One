12c327b61f90efbd8d1ad20d89f52913c1bd32ae
Rz Rasel <emailrzrasel@gmail.com>
2021-06-13 13:58:13 +0600

    Rz Rasel - Flutter Tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,58 @@ class WebViewTutorial extends StatefulWidget {
 }
 
 class WebViewTutorialState extends State<WebViewTutorial> {
+  @override
+  Widget build(BuildContext context) {
+    WebViewController webViewController;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WillPopScope(
+        onWillPop: () async {
+          String url = await webViewController.currentUrl();
+          if(url == "https://github.com/rzrasel") {
+            return true;
+          } else {
+            webViewController.goBack();
+            return false;
+          }
+        },
+        child: Scaffold(
+          body: Container(
+            child: SafeArea(
+              child: WebView(
+                initialUrl: "https://github.com/rzrasel",
+                javascriptMode: JavascriptMode.unrestricted,
+                onWebViewCreated: (WebViewController argWebViewController) {
+                  webViewController = argWebViewController;
+                },
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+/*
+// Rz Rasel Tutorial (Rashed - Uz - Zaman)
+// Import flutter material package
+import "package:flutter/material.dart";
+import "package:webview_flutter/webview_flutter.dart";
+
+void main() => runApp(WebViewTutorial());
+
+class WebViewTutorial extends StatefulWidget {
+  @override
+  WebViewTutorialState createState() => WebViewTutorialState();
+}
+
+class WebViewTutorialState extends State<WebViewTutorial> {
   WebViewController webViewController;
   @override
   Widget build(BuildContext context) {
@@ -44,4 +96,4 @@ class WebViewTutorialState extends State<WebViewTutorial> {
   }
 }
 
-//webview_flutter 2.0.8 => https://pub.dev/packages/webview_flutter
+//webview_flutter 2.0.8 => https://pub.dev/packages/webview_flutter*/
