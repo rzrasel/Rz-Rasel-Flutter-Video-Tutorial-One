2c36c8770e7f3d478b79c758e9aa129802b93aca
Rz Rasel <emailrzrasel@gmail.com>
2021-06-13 12:33:50 +0600

    Rz Rasel - Flutter Tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -16,15 +16,26 @@ class WebViewTutorialState extends State<WebViewTutorial> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-          child: SafeArea(
-            child: WebView(
-              initialUrl: "https://github.com/rzrasel",
-              javascriptMode: JavascriptMode.unrestricted,
-              onWebViewCreated: (WebViewController argWebViewController) {
-                webViewController = argWebViewController;
-              },
+      home: WillPopScope(
+        onWillPop: () async {
+          String url = await webViewController.currentUrl();
+          if(url == "https://github.com/rzrasel") {
+            return true;
+          } else {
+            webViewController.goBack();
+            return false;
+          }
+      },
+        child: Scaffold(
+          body: Container(
+            child: SafeArea(
+              child: WebView(
+                initialUrl: "https://github.com/rzrasel",
+                javascriptMode: JavascriptMode.unrestricted,
+                onWebViewCreated: (WebViewController argWebViewController) {
+                  webViewController = argWebViewController;
+                },
+              ),
             ),
           ),
         ),
@@ -32,3 +43,5 @@ class WebViewTutorialState extends State<WebViewTutorial> {
     );
   }
 }
+
+//webview_flutter 2.0.8 => https://pub.dev/packages/webview_flutter
