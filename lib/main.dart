aa91654e620b902133a5f772dd98e1f82334a9a1
Rz Rasel <emailrzrasel@gmail.com>
2021-06-13 12:01:16 +0600

    Rz Rasel - Flutter Tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // Rz Rasel Tutorial (Rashed - Uz - Zaman)
 // Import flutter material package
 import "package:flutter/material.dart";
+import "package:webview_flutter/webview_flutter.dart";
 
 void main() => runApp(WebViewTutorial());
 
@@ -10,8 +11,21 @@ class WebViewTutorial extends StatefulWidget {
 }
 
 class WebViewTutorialState extends State<WebViewTutorial> {
+  WebViewController webViewController;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          child: WebView(
+            initialUrl: "https://github.com/rzrasel",
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController argWebViewController) {
+              webViewController = argWebViewController;
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
