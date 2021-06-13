761cb603facb136d53bff0408540d95bcd244efa
Rz Rasel <emailrzrasel@gmail.com>
2021-06-13 11:49:28 +0600

    Rz Rasel - Flutter Tutorial

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
-# Rz Rasel - Flutter Tutorial - 1
-Rz Rasel - Flutter Tutorial
-
-Tutorial List:
-* [Navigate To Another Screen One](https://github.com/rzrasel/Flutter-Tutorial-Rz-Rasel/tree/Flutter-Tutorial-Navigate-To-Another-Screen-One)
-* [Splash Screen One](https://github.com/rzrasel/Flutter-Tutorial-Rz-Rasel/tree/Flutter-Tutorial-Splash-Screen-One)
+# Rz Rasel - Flutter Tutorial
+  This tutorial we show how to integrate WebView
--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,17 @@
 // Rz Rasel Tutorial (Rashed - Uz - Zaman)
 // Import flutter material package
 import "package:flutter/material.dart";
-import "SecondPage.dart";
 
-void main() => runApp(MyApp());
+void main() => runApp(WebViewTutorial());
 
-class MyApp extends StatelessWidget {
+class WebViewTutorial extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Navigation Control - Rz Rasel Tutorial",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: FirstPage(),
-    );
-  }
+  WebViewTutorialState createState() => WebViewTutorialState();
 }
 
-
-class FirstPage extends StatelessWidget {
+class WebViewTutorialState extends State<WebViewTutorial> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("First Page - Rz Rasel Tutorial"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Go to Second Page"),
-          style: ElevatedButton.styleFrom(
-            primary: Colors.blue,
-            onPrimary: Colors.white,
-          ),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => SecondPage(),
-              )
-            );
-          },
-        ),
-      ),
-    );
+    return Container();
   }
 }
